fd25b4c85fa54a264b8e18a7be2136df0c9fb54a
UNCROOS <any492266@gmail.com>
2024-07-13 01:28:47 +0900

    init

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 1), () {
+    Future.delayed(Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => ConcentrationPage()),
